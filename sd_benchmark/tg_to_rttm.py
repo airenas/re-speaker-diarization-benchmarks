a72885fcab356229cbb4addb60508b4869b424c9
Airenas Vaičiūnas <airenass@gmail.com>
2023-10-10 15:38:50 +0300

    Read 2 intervals data from textgrids

--- sd_benchmark/tg_to_rttm.py
+++ sd_benchmark/tg_to_rttm.py
@@ -7,6 +7,32 @@ import textgrids
 from sd_benchmark.logger import logger
 
 
+class RTTMData:
+    def __init__(self, label, start, dur):
+        self.label = label
+        self.dur = dur
+        self.start = start
+
+
+def try_join(rttm, r):
+    start = r.start
+    for cr in rttm:
+        if cr.label == r.label and abs(cr.start + cr.dur - start) <= 0.01:  # 10ms
+            dur = r.start + r.dur - cr.start
+            if dur > cr.dur:
+                cr.dur = dur
+            return True
+    return False
+
+
+def join_same_speaker(rttm):
+    res = []
+    for r in rttm:
+        if not try_join(res, r):
+            res.append(r)
+    return res
+
+
 def main(argv):
     logger.info("Starting")
     parser = argparse.ArgumentParser(description="Converts text grid to rttm")
@@ -26,22 +52,36 @@ def main(argv):
     logger.info(f"In: {file}")
     logger.info(f"Out: {out_file}")
 
+    res = []
+
+    def add_annotation(ann):
+        start_time = ann.xmin
+        duration = ann.xmax - start_time
+        label = str(ann.text)
+        if label and not label.startswith("_"):
+            res.append(RTTMData(label=label, start=start_time, dur=duration))
+
     try:
         file_name, _ = os.path.splitext(os.path.basename(file))
-        rttm_lines = []
         grid = textgrids.TextGrid(file)
         for annotation in grid['S']:
-            start_time = annotation.xmin
-            duration = annotation.xmax - start_time
-            label = annotation.text
+            add_annotation(annotation)
+        for annotation in grid['A']:
+            add_annotation(annotation)
 
-            if label and label != "_S":
-                rttm_line = f"SPEAKER {file_name} 1 {start_time:.3f} {duration:.3f} <NA> <NA> {label} <NA> <NA>"
-                rttm_lines.append(rttm_line)
+        res = sorted(res, key=lambda d: d.start)
+
+        res = join_same_speaker(res)
+
+        rttm_lines = []
+        for r in res:
+            rttm_line = f"SPEAKER {file_name} 1 {r.start:.3f} {r.dur:.3f} <NA> <NA> {r.label} <NA> <NA>"
+            rttm_lines.append(rttm_line)
 
         with open(out_file, "w") as file:
             [file.write(line + '\n') for line in rttm_lines]
         logger.info("done")
+
     except Exception as e:
         logger.error(f"Error processing file {args.input}: {str(e)}")
         sys.exit(1)
